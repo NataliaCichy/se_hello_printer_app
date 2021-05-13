23ae0cac75b5dc474c88c727b1f5afa8893ff379
Natalia Cichy <natalia.cichy@gmail.com>
2021-05-13 23:04:02 +0200

    poprawiono blad

--- hello_world/views.py
+++ hello_world/views.py
@@ -3,7 +3,7 @@ from hello_world.formater import get_formatted
 from hello_world.formater import SUPPORTED, PLAIN
 from flask import request
 
-moje_imie = "NataliaG"
+moje_imie = "NataliaC"
 msg = "Hello World!"
 
 
